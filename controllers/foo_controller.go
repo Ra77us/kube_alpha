3c00437d9cbddb8640851dac343c8f56c53cf1e6
Ra77us <michalszczurek@o2.pl>
2023-04-02 20:16:10 +0200

    a

--- controllers/foo_controller.go
+++ controllers/foo_controller.go
@@ -1,57 +1,43 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	alphav1 "kubiki/alpha/api/v1"
 )
 
-// TestReconciler reconciles a Test object
-type TestReconciler struct {
+// FooReconciler reconciles a Foo object
+type FooReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=alpha.kubiki,resources=tests,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=alpha.kubiki,resources=tests/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=alpha.kubiki,resources=tests/finalizers,verbs=update
+// RBAC permissions to monitor foo custom resources
+//+kubebuilder:rbac:groups=tutorial.my.domain,resources=foos,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=tutorial.my.domain,resources=foos/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=tutorial.my.domain,resources=foos/finalizers,verbs=update
+
+// RBAC permissions to monitor pods
+//+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Test object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *FooReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 	log.Info("reconciling foo custom resource")
 
-	// Get the Tst resource that triggered the reconciliation request
-	var foo alphav1.Test
+	// Get the Foo resource that triggered the reconciliation request
+	var foo alphav1.Foo
 	if err := r.Get(ctx, req.NamespacedName, &foo); err != nil {
 		log.Error(err, "unable to fetch Foo")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
@@ -86,7 +72,7 @@ func (r *FooReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 // SetupWithManager sets up the controller with the Manager.
 func (r *FooReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&alphav1.Test{}).
+		For(&alphav1.Foo{}).
 		Watches(
 			&source.Kind{Type: &corev1.Pod{}},
 			handler.EnqueueRequestsFromMapFunc(r.mapPodsReqToFooReq),
--- main.go
+++ main.go
@@ -88,12 +88,11 @@ func main() {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
-
-	if err = (&controllers.TestReconciler{
+	if err = (&controllers.FooReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Test")
+		setupLog.Error(err, "unable to create controller", "controller", "Foo")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
